36fa2fed2fb88e18a2f689310302a7d226303aaf
tanvir-coderboy <t87373654@gmail.com>
2025-11-26 15:53:45 +0600

    Sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -18,7 +18,10 @@
                     <a href="{{ route('admin.dashboard') }}" class="nav-link {{ Route::is('admin.dashboard') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
-                            Dashboard
+                            {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('Dashboard') 
+                            : 'Dashboard' 
+                            }}
                         </p>
                     </a>
                 </li>
@@ -31,7 +34,10 @@
                     <a href="{{ route('admin.settings.index') }}" class="nav-link {{ Route::is('admin.settings.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-cog"></i>
                         <p>
-                            Settings
+                            {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('Settings') 
+                            : 'Settings' 
+                            }}
                         </p>
                     </a>
                 </li>
@@ -40,73 +46,107 @@
                 <li class="nav-item">
                     <a href="{{ route('admin.department.index') }}" class="nav-link {{ Route::is('admin.department.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-building"></i>
-                        <p>Department</p>
+                        <p>
+                            {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('Department') 
+                            : 'Department' 
+                            }}
+                        </p>
                     </a>
                 </li>
 
                 <li class="nav-item">
                     <a href="{{ route('admin.position.index') }}" class="nav-link {{ Route::is('admin.position.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-briefcase"></i>
-                        <p>Position</p>
+                        <p>
+                            {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('Position') 
+                            : 'Position' 
+                            }}
+                        </p>
                     </a>
                 </li>
 
-                 <li class="nav-item">
+                <li class="nav-item">
                     <a href="{{ route('admin.staff.index') }}" class="nav-link {{ Route::is('admin.staff.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-users"></i>
-                        <p>Employee</p>
+                        <p>
+                            {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('Employee') 
+                            : 'Employee' 
+                            }}
+                        </p>
                     </a>
                 </li>
 
                 <li class="nav-item">
                     <a href="{{ route('admin.customers.index') }}" class="nav-link {{ Route::is('admin.customers.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-user-friends"></i>
-                        <p>Customers</p>
+                        <p>
+                            {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('Customers') 
+                            : 'Customers' 
+                            }}
+                        </p>
                     </a>
                 </li>
 
                 <li class="nav-item">
                     <a href="{{ route('admin.salary.index') }}" class="nav-link {{ Route::is('admin.salary.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-money-bill-wave"></i>
-                        <p>Salary</p>
+                        <p>
+                            {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('Salary') 
+                            : 'Salary' 
+                            }}
+                        </p>
                     </a>
                 </li>
-               
+
 
 
 
                 <!-- Role Managment  -->
-               @can(['view role','view user'])
+                @can(['view role','view user'])
                 {{--<li class="nav-item {{ Route::is('admin.roles.*') || Route::is('admin.users.*') ? 'menu-open' : '' }}">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-user-circle"></i>
-                        <p>
-                            User Management
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        @can('view user')
-                        <li class="nav-item">
-                            <a href="{{ route('admin.users.index') }}" class="nav-link {{ Route::is('admin.users.*') ? 'active' : '' }}">
-                                <i class="fa fa-angle-right nav-icon"></i>
-                                <p>User</p>
-                            </a>
-                        </li>
-                        @endcan
-                        @can('view role')
-                        <li class="nav-item">
-                            <a href="{{ route('admin.roles.index') }}" class="nav-link {{ Route::is('admin.roles.*') ? 'active' : '' }}">
-                                <i class="fa fa-angle-right nav-icon"></i>
-                                <p>Roles</p>
-                            </a>
-                        </li>
-                        @endcan
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-user-circle"></i>
+                    <p>
+                        {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('User Management') 
+                            : 'User Management' 
+                            }}
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    @can('view user')
+                    <li class="nav-item">
+                        <a href="{{ route('admin.users.index') }}" class="nav-link {{ Route::is('admin.users.*') ? 'active' : '' }}">
+                            <i class="fa fa-angle-right nav-icon"></i>
+                            {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('Users') 
+                            : 'Users' 
+                            }}
+                        </a>
+                    </li>
+                    @endcan
+                    @can('view role')
+                    <li class="nav-item">
+                        <a href="{{ route('admin.roles.index') }}" class="nav-link {{ Route::is('admin.roles.*') ? 'active' : '' }}">
+                            <i class="fa fa-angle-right nav-icon"></i>
+                            {{ session('locale', 'en') == 'ar' 
+                            ? \App\Helpers\TranslateHelper::toArabic('Roles') 
+                            : 'Roles' 
+                            }}
+                        </a>
+                    </li>
+                    @endcan
 
-                    </ul>
+                </ul>
                 </li>--}}
                 @endcan
-           
+
 
         </nav>
         <!-- /.sidebar-menu -->
